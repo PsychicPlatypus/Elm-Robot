f7d05cf0e80b99dae8d12199a5dd469fd2e0edad
PsychicPlatypus <dzenismadzovic@gmail.com>
2022-04-06 01:25:20 +0200

    init commit

---
+++ src/Robot.elm
@@ -0,0 +1,99 @@
+module Robot exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (style, type_, value)
+import Html.Events exposing (onClick, onInput)
+import RobotFuncs exposing (..)
+import String exposing (toInt)
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
+
+
+turnToInt : String -> Int
+turnToInt str =
+    str
+        |> toInt
+        |> Maybe.withDefault 0
+
+
+
+-- Model
+
+
+type alias Model =
+    { x : Int
+    , y : Int
+    , commands : String
+    , facing : String
+    }
+
+
+init : Model
+init =
+    { x = 0
+    , y = 0
+    , commands = ""
+    , facing = "N"
+    }
+
+
+
+-- Subscriptions
+-- Update
+
+
+type Msg
+    = SetX String
+    | SetY String
+    | SetCommands String
+    | ButtonPressed
+
+
+
+-- Run
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        SetX num ->
+            { model | x = turnToInt num }
+
+        SetY num ->
+            { model | y = turnToInt num }
+
+        SetCommands cmds ->
+            { model | commands = cmds }
+
+        ButtonPressed ->
+            let
+                func =
+                    RobotFuncs.execute_orders model.x model.y model.commands "English" model.facing
+            in
+            { model | x = func.x, y = func.y, facing = func.dir }
+
+
+
+-- Run ->
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div [ style "display" "flex", style "flex-direction" "column", style "justify-content" "center", style "align-items" "center" ]
+        [ h1 [] [ text "Robot" ]
+        , div []
+            [ input [ type_ "Text", onInput SetX, value (Debug.toString model.x) ] []
+            , input [ type_ "Text", onInput SetY, value (Debug.toString model.y) ] []
+            , input [ type_ "Text", onInput SetCommands, value model.commands ] []
+            ]
+        , button [ onClick ButtonPressed ] [ text "Run" ]
+        ]
---
+++ src/RobotFuncs.elm
@@ -0,0 +1,121 @@
+module RobotFuncs exposing (execute_orders, fromJust)
+
+import List exposing (drop, head, take)
+import String exposing (left, length, right, slice, toUpper)
+
+
+fromJust : Maybe a -> a
+fromJust x =
+    case x of
+        Just y ->
+            y
+
+        Nothing ->
+            Debug.todo "error: fromJust Nothing"
+
+
+turn_left : List String -> List String
+turn_left dir_list =
+    let
+        x =
+            take 1 dir_list
+
+        y =
+            drop 1 dir_list
+
+        return_list =
+            y ++ x
+    in
+    return_list
+
+
+turn_right : List String -> List String
+turn_right dir_list =
+    let
+        x =
+            take 3 dir_list
+
+        y =
+            drop 3 dir_list
+
+        return_list =
+            y ++ x
+    in
+    return_list
+
+
+facing : String -> List String
+facing dir =
+    case dir of
+        "N" ->
+            [ "N", "E", "S", "W" ]
+
+        "E" ->
+            [ "E", "S", "W", "N" ]
+
+        "S" ->
+            [ "S", "W", "N", "E" ]
+
+        _ ->
+            [ "W", "N", "E", "S" ]
+
+
+execute_orders : Int -> Int -> String -> String -> String -> { x : Int, y : Int, dir : String }
+execute_orders x y commands lang dir =
+    let
+        moves =
+            case lang of
+                "English" ->
+                    "RLF"
+
+                "French" ->
+                    "HVG"
+
+                "Swedish" ->
+                    "DGT"
+
+                _ ->
+                    Debug.todo "Add more Languages"
+
+        fw =
+            right 1 moves
+
+        directions =
+            facing dir
+
+        rght =
+            left 1 moves
+
+        lft =
+            slice 1 2 moves
+
+        first_move =
+            left 1 (toUpper commands)
+
+        rest =
+            slice 1 (length commands) (toUpper commands)
+    in
+    if first_move == "" then
+        { x = x, y = y, dir = fromJust (head directions) }
+
+    else if first_move == fw then
+        if dir == "N" then
+            execute_orders x (y - 1) rest lang dir
+
+        else if dir == "E" then
+            execute_orders (x + 1) y rest lang dir
+
+        else if dir == "S" then
+            execute_orders x (y + 1) rest lang dir
+
+        else
+            execute_orders (x - 1) y rest lang dir
+
+    else if first_move == lft then
+        execute_orders x y rest lang (fromJust (head (turn_left directions)))
+
+    else if first_move == rght then
+        execute_orders x y rest lang (fromJust (head (turn_right directions)))
+
+    else
+        Debug.todo "branch '_' not implemented"
